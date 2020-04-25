2a02c0af932b83b59534b2fcbfe35893108d1c8d
Luis Constante <luisconstante@LConstanteOSX.local>
2020-04-24 17:22:52 -0700

    deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MusicControls } from '@ionic-native/music-controls/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    MusicControls,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MusicControls } from '@ionic-native/music-controls/ngx';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -11,8 +12,9 @@ export class AudioService {
   audioMemAllocation: HTMLAudioElement = null;
   audioIsPlaying: Boolean = false;
 
-  constructor() {
+  constructor(private musicControls: MusicControls) {
     this.loadAudioEssentials();
+    // this.cordovaAudioInit();
 
     // load mp3 stream into DOM
     this.audioSource.subscribe(audioElem => {
@@ -24,13 +26,14 @@ export class AudioService {
     this.audioSourceState.subscribe(isPlaying => {
       this.audioIsPlaying = isPlaying;
     });
+
   }
 
   playSource() {
     this.audioMemAllocation.play();
     this.audioSourceState.next(true);
   }
-  
+
   stopSource() {
     this.audioMemAllocation.pause();
     this.audioSourceState.next(false);
@@ -40,12 +43,47 @@ export class AudioService {
     const audioElem = new Audio('https://onlineradiobox.com/json/us/nonstopoldies/play?platform=web');
     audioElem.style.visibility = 'hidden';
     audioElem.style.position = 'absolute';
-    
+
     this.audioSource.next(audioElem);
   }
 
   destroyAudioEssentials() {
     this.audioMemAllocation.remove();
     this.audioSourceState.next(false);
+  }
+
+  cordovaAudioInit() {
+    try {
+      this.musicControls.create({
+        track: 'Relaxation Time',        // optional, default : ''
+        artist: 'Viscira',                       // optional, default : ''
+
+        // album       : 'Absolution',     // optional, default: ''
+        hasScrubbing: false, // enable scrubbing from control center and lockscreen progress bar, optional
+
+        // Android only, optional
+        // text displayed in the status bar when the notification (and the ticker) are updated, optional
+        ticker: 'Viscira Relax App',
+        // All icons default to their built-in android equivalents
+        playIcon: 'media_play',
+        pauseIcon: 'media_pause',
+      });
+
+      this.musicControls.subscribe().subscribe(action => {
+        const message = JSON.parse(action).message;
+        switch (message) {
+          case 'music-controls-pause':
+            this.stopSource();
+            break;
+          case 'music-controls-play':
+            this.playSource();
+            break;
+        }
+      });
+
+      this.musicControls.listen();
+    } catch (e) {
+      alert('web browser or not compatible with device.');
+    }
   }
 }
